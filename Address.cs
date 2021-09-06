1148c013692f3a3c2e7ae9e9e3413121d6905636
JoakimTell <62691952+JoakimTell@users.noreply.github.com>
2021-09-06 11:53:40 +0200

    Added new Adddress class. Variables and properties in Address.

---
+++ Address.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RealEstateAgent
+{
+    public class Address
+    {
+        private String street;
+        private String zipCode;
+        private String city;
+        private Countries country;
+
+        public String Street
+        {
+            get { return street; }
+
+            set { street = value; }
+        }
+
+        public String ZipCode
+        {
+            get { return zipCode; }
+
+            set { zipCode = value; }
+        }
+
+        public String City
+        {
+            get { return city; }
+
+            set { city = value; }
+        }
+
+        public Countries Country
+        {
+            get { return country; }
+
+            set { country = value; }
+        }
+    }
+}
