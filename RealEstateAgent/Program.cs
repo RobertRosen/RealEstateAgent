4658108cf108243146017c67a9411270430abf7d
RobertRosen <62375397+RobertRosen@users.noreply.github.com>
2021-10-07 20:24:08 +0200

    String converter.

--- RealEstateAgent/Program.cs
+++ RealEstateAgent/Program.cs
@@ -8,6 +8,8 @@ using System.Windows.Forms;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Xml.Serialization;
+using System.Diagnostics;
+using UtilitiesLib;
 
 namespace RealEstateApp
 {
@@ -19,6 +21,18 @@ namespace RealEstateApp
         [STAThread]
         static void Main()
         {
+            //--------------------------Testing Utilities--------------------------//
+            Debug.WriteLine(StringConverter.ParseStringToInteger("12"));
+            Debug.WriteLine(StringConverter.ParseStringToDouble("12"));
+            Debug.WriteLine(StringConverter.ParseStringToInteger("12", 10, 15));
+            Debug.WriteLine(StringConverter.ParseStringToDouble("12", 10, 15));
+
+            Debug.WriteLine(StringConverter.ParseStringToInteger("tt"));
+            Debug.WriteLine(StringConverter.ParseStringToDouble("tt"));
+            Debug.WriteLine(StringConverter.ParseStringToInteger("9", 10, 15));
+            Debug.WriteLine(StringConverter.ParseStringToDouble("32", 10, 15));
+            //---------------------------------------------------------------------//
+
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
---
+++ UtilitiesLib/StringConverter.cs
@@ -0,0 +1,51 @@
+﻿namespace UtilitiesLib
+{
+    public static class StringConverter
+    {
+        public static int ParseStringToInteger(string strNumber)
+        {
+            int defaultValue = 0;
+            int parsedValue = defaultValue;
+
+            if (int.TryParse(strNumber, out parsedValue))
+                return parsedValue;
+
+            return defaultValue;
+        }
+
+        public static double ParseStringToDouble(string strNumber)
+        {
+            double defaultValue = 0d;
+            double parsedValue = defaultValue;
+
+            if (double.TryParse(strNumber, out parsedValue))
+                return parsedValue;
+
+            return parsedValue;
+        }
+
+        public static int ParseStringToInteger(string strNumber, int lowerLimit, int upperLimit)
+        {
+            int defaultValue = 0;
+            int parsedValue = defaultValue;
+
+            if (int.TryParse(strNumber, out parsedValue))
+                if ((parsedValue >= lowerLimit) && (parsedValue <= upperLimit))
+                    return parsedValue;
+
+            return defaultValue;
+        }
+
+        public static double ParseStringToDouble(string strNumber, double lowerLimit, double upperLimit)
+        {
+            double defaultValue = 0d;
+            double parsedValue = defaultValue;
+
+            if (double.TryParse(strNumber, out parsedValue))
+                if ((parsedValue >= lowerLimit) && (parsedValue <= upperLimit))
+                    return parsedValue;
+
+            return defaultValue;
+        }
+    }
+}
